a022e3a2be2f63413aeb5eaf29edf54b50398412
sstanytska <stanytskasofia@gmail.com>
2019-04-30 14:01:22 -0500

    changes jira-deployment.tf and added variables.tf, jira-ns.tf

--- google_jira/jira-deployment.tf
+++ google_jira/jira-deployment.tf
@@ -1,40 +1,99 @@
-resource "kubernetes_deployment" "terraform-jira" {
+resource "kubernetes_persistent_volume_claim" "jira-pvc" {
   metadata {
-    name      = "terraform-jira"
-    labels    = { app = "jira-deployment" }
-    namespace = "tools"
-   }
+    name      = "jira-pvc"
+    namespace = "${var.namespace}"
+
+    labels {
+      app = "jira-deployment"
+    }
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests {
+        storage = "10Gi"
+      }
+    }
+
+    storage_class_name = "standard"
+  }
+}
+
+resource "kubernetes_deployment" "jira-deployment" {
+  metadata {
+    name      = "jira-deployment"
+    namespace = "${var.namespace}"
+
+    labels {
+      app = "jira-deployment"
+    }
+  }
 
   spec {
     replicas = 1
 
     selector {
       match_labels {
-        app = "jira-pod"
+        app = "jira-deployment"
       }
     }
 
     template {
       metadata {
         labels {
-          app = "jira-pod"
+          app = "jira-deployment"
         }
       }
 
       spec {
-        container {
-          image = "gcr.io/hightowerlabs/jira:7.3.6-standalone"
-          name  = "jira"
-          volume_mount {
-             name       = "jira-home"
-             mount_path = "/opt/jira-home"
-           }
+        volume {
+          name = "jira-pvc"
+
+          persistent_volume_claim {
+            claim_name = "jira-pvc"
           }
-           volume {
-          name = "jira-home"
-        }
         }
 
+        container {
+          image = "gcr.io/hightowerlabs/jira:7.3.6-standalone"
+          name  = "jira-deployment"
+
+          port {
+            container_port = 8080
+            protocol       = "TCP"
+          }
+
+          volume_mount {
+            name       = "jira-pvc"
+            mount_path = "/var/lib/jira"
+          }
+
+          image_pull_policy = "IfNotPresent"
+        }
       }
     }
   }
+}
+
+resource "kubernetes_service" "jira-service" {
+  metadata {
+    name      = "jira-service"
+    namespace = "${var.namespace}"
+  }
+
+  spec {
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 8080
+    }
+
+    selector {
+      app = "jira-deployment"
+    }
+
+    type = "LoadBalancer"
+  }
+}
