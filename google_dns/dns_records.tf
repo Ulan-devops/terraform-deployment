a84fdebde3b2a08b7300eb3aafda1b4878bcdf47
Akmal Kadyrov <akmalkadyrov@Akmals-MacBook-Pro.local>
2019-05-15 11:54:58 -0600

    storage dns added

--- google_dns/dns_records.tf
+++ google_dns/dns_records.tf
@@ -81,3 +81,12 @@ resource "google_dns_record_set" "prod" {
 
   rrdatas = ["${var.ip_domain}"]
 }
+
+resource "google_dns_record_set" "storage" {
+  managed_zone = "${google_dns_managed_zone.fuchicorp.name}"
+  name = "storage.${google_dns_managed_zone.fuchicorp.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  rrdatas = ["${var.ip_domain}"]
+}
